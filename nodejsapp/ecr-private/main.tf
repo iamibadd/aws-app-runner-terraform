5eafcfd14e5e0849724773fe9fdcc37ef854155a
Ibad Shah <iamibadd@gmail.com>
2022-12-18 00:25:35 +0500

    Subject: Added AWS App Runner deployments for backend and frontend repos.

--- nodejsapp/ecr-private/main.tf
+++ nodejsapp/ecr-private/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {}
 
 module "state" {
-  source = "./modules/state"
+  source = "modules/state"
 }
 
 module "ecr" {
-  source = "./modules/ecr"
+  source = "modules/ecr"
 }
 
 module "app_runner" {
-  source       = "./modules/app-runner"
+  source       = "modules/app-runner"
   ecr_repo_url = module.ecr.ecr_repo_url
 }
